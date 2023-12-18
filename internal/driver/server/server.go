863dd35d80fc04c792799f531ff0e7375032ef83
otakakot <kotaro.otaka@gmail.com>
2023-12-19 00:37:16 +0900

    first commit

--- internal/driver/server/server.go
+++ internal/driver/server/server.go
@@ -53,9 +53,9 @@ func (srv *Server) Run() {
 
 	slog.Info("start server shutdown")
 
-	ctx, cansel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
-	defer cansel()
+	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
 		panic(err)
